383e70798589cd2ee9387804805df4f5aaa57de6
Victor Peinado <vpeinado263@gmail.com>
2026-07-25 18:38:36 -0300

    Eliminar elemento duplicado

--- app/Http/Controllers/ProcedimientoController.php
+++ app/Http/Controllers/ProcedimientoController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Procedimiento;
 use Illuminate\Http\Request;
 
--- app/Http/Controllers/SolicitudController.php
+++ app/Http/Controllers/SolicitudController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Solicitud;
-use Illuminate\Http\Request;
 
 class SolicitudController extends Controller
 {
