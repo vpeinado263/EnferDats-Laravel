14026ad0a1f165d8723136a9fae9856f4e4fa6a9
Victor Peinado <vpeinado263@gmail.com>
2026-07-25 13:42:16 -0300

    Crear controladores e implementar metodos

---
+++ app/Http/Controllers/ProcedimientoController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Procedimiento;
+use Illuminate\Http\Request;
+
+class ProcedimientoController extends Controller
+{
+    
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nombre' => 'required|string|max:255',
+            'costo' => 'required|numeric|min:0',
+        ]);
+    
+        Procedimiento::create($request->all());
+        return redirect()->route('dashboard')->with('success', 'Procedimiento creado.');
+    }
+
+    public function update(Request $request, $id)
+    
+    {
+        $procedimiento = Procedimiento::findOrFail($id);
+        $request->validate([
+        'nombre' => 'required|string|max:255',
+        'costo' => 'required|numeric|min:0',]);
+        $procedimiento->update($request->all());
+        
+        return redirect()->route('dashboard')->with('success', 'Procedimiento actualizado.');
+    }
+
+    
+    public function destroy($id)
+    {
+        Procedimiento::findOrFail($id)->delete();
+        return redirect()->route('dashboard')->with('success', 'Procedimiento eliminado.');
+    }
+}
