359c7ba7628a67823b7c517fbca625a36605ba32
Victor Peinado <vpeinado263@gmail.com>
2026-07-25 18:35:59 -0300

    Corregir propiedad

--- app/Models/Insumo.php
+++ app/Models/Insumo.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Insumo extends Model
 {
     use HasFactory;
+    protected $fillable = ['nombre', 'precio'];
 }
-protected $fillable = ['nombre', 'precio'];
