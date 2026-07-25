bda6ce9a32c62bd3e9c8a1216d6961e5caba68b4
Victor Peinado <vpeinado263@gmail.com>
2026-07-25 20:32:40 -0300

    Agregar cambios a web.php rutas definidas

--- routes/web.php
+++ routes/web.php
@@ -3,25 +3,34 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InicioController;
 use App\Http\Controllers\ServiciosController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProcedimientoController;
 use App\Http\Controllers\InsumoController;
 use App\Http\Controllers\SolicitudController;
-use App\Http\Controllers\DashboardController;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+
+// Página de inicio 
 Route::get('/', [InicioController::class, 'index'])->name('inicio');
 
+// Página de servicios
 Route::get('/servicios', [ServiciosController::class, 'index'])->name('servicios');
 
+// Página de contacto 
 Route::get('/contacto', [InicioController::class, 'contacto'])->name('contacto');
 
-// Dashboard usando el controlador
+// Dashboard 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-// CRUD para procedimientos (todas las rutas excepto 'show')
+// CRUD para procedimientos 
 Route::resource('procedimientos', ProcedimientoController::class)->except(['show']);
 
 // CRUD para insumos 
 Route::resource('insumos', InsumoController::class)->except(['show']);
 
-// Ruta para guardar solicitudes
+// Guardar solicitudes
 Route::post('/solicitudes', [SolicitudController::class, 'store'])->name('solicitudes.store');
