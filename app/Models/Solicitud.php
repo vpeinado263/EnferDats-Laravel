557486d7589a6e10f770fbe38468f3151956c610
Victor Peinado <vpeinado263@gmail.com>
2026-07-25 18:35:10 -0300

    Corregir propiedad

--- app/Models/Solicitud.php
+++ app/Models/Solicitud.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Solicitud extends Model
 {
     use HasFactory;
+    protected $fillable = ['institucion', 'tipo_institucion', 'provincia', 'responsable', 'cargo', 'email', 'telefono', 'objetivos', 'necesidades'];
 }
-protected $fillable = ['institucion', 'tipo_institucion', 'provincia', 'responsable', 'cargo', 'email', 'telefono', 'objetivos', 'necesidades'];
