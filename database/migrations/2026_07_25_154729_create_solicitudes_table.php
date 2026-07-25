14f57fc2538a7ec786121d87f45ed4bdbbd0db44
Victor Peinado <vpeinado263@gmail.com>
2026-07-25 13:04:39 -0300

    Crear migraciones para las tablas procedimiento, insumo, solicitudes

---
+++ database/migrations/2026_07_25_154729_create_solicitudes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('solicitudes', function (Blueprint $table) {
+            $table->id();
+            $table->string('institucion');
+            $table->string('tipo_institucion')->nullable();
+            $table->string('provincia')->nullable();
+            $table->string('responsable');
+            $table->string('cargo')->nullable();
+            $table->string('email');
+            $table->string('telefono')->nullable();
+            $table->text('objetivos')->nullable(); 
+            $table->text('necesidades')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('solicitudes');
+    }
+};
